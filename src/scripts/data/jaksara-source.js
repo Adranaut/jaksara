ff279df37be5e743d8c82b0a37a7f55d1e6f4a62
Aditia Prabowo <aditiaprabowo3@gmail.com>
2024-06-08 14:25:12 +0700

    update text ui

--- src/scripts/data/jaksara-source.js
+++ src/scripts/data/jaksara-source.js
@@ -33,35 +33,9 @@ class JaksaraSource {
     }
 
     static async quiz() {
-        try {
-            const response = await fetch(API_ENDPOINT.QUIZ);
-
-            // Log status dan headers dari respons
-            console.log('API Response Status:', response.status);
-            console.log('API Response Headers:', response.headers);
-
-            // Cek jika responsnya ok
-            if (!response.ok) {
-                console.error('API response not ok');
-                return null;
-            }
-
-            const responseJson = await response.json();
-
-            // Log respons JSON lengkap
-            console.log('API Response JSON:', responseJson);
-
-            // Mengakses data.quizs dari respons JSON
-            if (responseJson.status === 'success' && responseJson.data && Array.isArray(responseJson.data.quizs)) {
-                return responseJson.data.quizs;
-            } else {
-                console.error('API response does not contain quizs array.');
-                return null;
-            }
-        } catch (error) {
-            console.error('Failed to fetch data from API:', error);
-            return null;
-        }
+        const response = await fetch(API_ENDPOINT.QUIZ);
+        const responseJson = await response.json();
+        return responseJson.quizs;
     }
 }
 
